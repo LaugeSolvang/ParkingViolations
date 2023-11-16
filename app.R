d5c9e13e8e1c3db133e2834c14d19296b3b3abf2
LaugeSolvang <lasol21@student.sdu.dk>
2023-11-16 11:20:37 +0100

    Added rshiny app for viewing violations on map

--- app.R
+++ app.R
@@ -1,49 +1,67 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(leaflet)
+library(sf)
+library(dplyr)
+library(RColorBrewer)
 
-# Define UI for application that draws a histogram
+
+# Read the data outside the server function to do it only once
+precincts <- st_read("Police Precincts\\geo_export_c2050280-78f2-4481-be68-d54c4e876c56.shp")
+violations <- read.csv("sampled_200k_rows.csv")
+
+violations_summary <- violations %>%
+  group_by(Violation.Precinct) %>%
+  summarize(Count = n())
+
+merged_data <- merge(precincts, violations_summary, by.x = "precinct", by.y = "Violation.Precinct")
+
+breaks_sqrt <- seq(sqrt(min(merged_data$Count)), sqrt(max(merged_data$Count)), length.out = 10) ^ 2
+palette_function <- colorBin(palette = brewer.pal(9, "Blues"), bins = breaks_sqrt, domain = merged_data$Count)
+
+# Define the UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  titlePanel("Parking Violations in New York Precincts"),
+  leafletOutput("map"),
+  verbatimTextOutput("info"),
+  
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# Define the server logic
+server <- function(input, output, session) {
+  
+  output$map <- renderLeaflet({
+    leaflet(merged_data) %>% 
+      addTiles() %>% 
+      addPolygons(
+        fillColor = ~palette_function(Count),
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlightOptions = highlightOptions(
+          color = "blue", weight = 2,
+          bringToFront = TRUE
+        ),
+        label = ~paste("Precinct:", precinct, "Violations:", Count),
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"
+        )
+      )
+  })
+  
+  observeEvent(input$map_shape_click, {
+    click <- input$map_shape_click
+    info <- merged_data[merged_data$precinct == click$id, ]
+    output$info <- renderText({
+      paste0("Precinct: ", info$precinct, "<br>",
+             "Parking Violations: ", info$Count)
     })
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run the app
+shinyApp(ui, server)
+
