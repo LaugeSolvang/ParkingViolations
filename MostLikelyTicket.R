c0d959d52d53290309febfa0a86fe2653bcbef19
Andreas2200 <31403296+Andreas2200@users.noreply.github.com>
2023-11-29 21:44:47 +0100

    Made plot into donut chart instead of pie chart

--- MostLikelyTicket.R
+++ MostLikelyTicket.R
@@ -18,7 +18,7 @@ mostLikelyTicketModuleServer <- function(id, data) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     rawData <- read.csv("sampled_200k_rows.csv", stringsAsFactors = TRUE)
-    data <- select(rawData, Violation.Code, Vehicle.Make)
+    data <- select(rawData, Violation.Description, Vehicle.Make)
 
     topTicketTypes <- reactive({
       data %>%
@@ -37,7 +37,7 @@ mostLikelyTicketModuleServer <- function(id, data) {
       req(input$selectedVehicleMake)
       data %>%
         filter(Vehicle.Make == input$selectedVehicleMake) %>%
-        group_by(Violation.Code) %>%
+        group_by(Violation.Description) %>%
         summarise(Count = n(), .groups = "drop") %>%
         arrange(desc(Count)) %>%
         top_n(10) %>%
@@ -46,12 +46,19 @@ mostLikelyTicketModuleServer <- function(id, data) {
 
     output$ticketCountPlot <- renderPlot({
       fd <- filteredData()
-      ggplot(fd, aes(x="", y=Count, fill = Violation.Code)) +
-        geom_bar(stat = "identity", width = 1) + 
+      color_palette <- c("#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a", "#b15928", "#a6cee3", "#b2df8a", "#fb9a99", "#fdbf6f")
+      fd$Violation.Description <- factor(fd$Violation.Description)
+
+      dc <- ggplot(fd, aes(x="", y=Count, fill = Violation.Description)) +
+        geom_bar(stat = "identity", width = 1, color = "white") +
+        geom_polygon(data = data.frame(x = c(-1, 1, 1, -1), y = c(-1, -1, 1, 1)), aes(x = x, y = y), fill = "white", color = "white", show.legend = FALSE) +
         coord_polar("y", start = 0) +
-        theme_void() + 
+        theme_void() +
         labs(fill = "Top 10 most likely violations") +
-        geom_text(aes(label = paste0(round(Percentage, 1),"%")), position = position_stack(vjust = 0.5))
+        geom_text(aes(label = paste0(round(Percentage, 1),"%")), position = position_stack(vjust = 0.5)) +
+        scale_fill_manual(values = setNames(color_palette, levels(fd$Violation.Description))) +
+        theme(legend.position = "bottom")
+      dc
     })
   })
 }
