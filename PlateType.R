ddbbef534d92c03212c6baa190ef294420d041cc
Asma5577 <94056599+Asma5577@users.noreply.github.com>
2023-12-22 11:58:42 +0000

    Update Annotation

--- PlateType.R
+++ PlateType.R
@@ -4,12 +4,10 @@ library(ggplot2)
 library(tidyr)
 library(plotly)
 
-
-
 plateTypeModuleUI <- function(id) {
   ns <- NS(id)
-  ui <- fluidPage(
-    titlePanel("Top Five Plate Types and Violation Counties"),
+  fluidPage(
+    titlePanel("Top Five Violation Counties and Vehicle Plate Types"),
     mainPanel(
       plotlyOutput(ns("groupedBarChart")) 
     )
@@ -21,15 +19,16 @@ plateTypeModuleServer <- function(id, data) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
+    # Read the data
     Raw_data <- read.csv("sampled_200k_rows.csv", stringsAsFactors = TRUE)
     
+    # Prepare the data
     data <- data.frame(
       Type = Raw_data$Plate.Type,
-      ViolationCode = violations$Violation.Code,
+      ViolationCode = Raw_data$Violation.Code,
       County = Raw_data$Violation.County
     )
     df_percentage <- transform(data, Percentage = (ViolationCode / sum(ViolationCode)) * 100)
-    
     
     # Determine the top five plate types and counties
     top_plate_types <- df_percentage %>%
@@ -42,13 +41,31 @@ plateTypeModuleServer <- function(id, data) {
       top_n(5, n) %>%
       pull(County)
     
+    
     # Filter data for only top plate types and violation counties
     filtered_data <- df_percentage %>%
-      filter(Type %in% top_plate_types & County %in% top_violation_counties)
+      filter(Type %in% top_plate_types & County %in% top_violation_counties) %>%
+      mutate(
+        County = case_when(
+          County == "BK" ~ "Brooklyn",
+          County == "BX" ~ "Bronx",
+          County == "K" ~ "Kings",
+          County == "NY" ~ "New York",
+          County == "Q" ~ "Queens",
+          TRUE ~ County
+        ), # Apply the new names for County
+        
+        Type = case_when(
+          Type == "PAS" ~ "Passenger",
+          Type == "COM" ~ "Commercial",
+          Type == "OMS" ~ "Rental",
+          Type == "OMT" ~ "Taxi",
+          Type == "SRF" ~ "Special Passenger",
+          TRUE ~ Type
+        ) # Correctly apply the new names for Type
+      )
     
-    #print(filtered_data)
-    
-    # Summarize the data for plotting
+
     summarized_data <- filtered_data %>%
       count(Type, County) %>%
       pivot_wider(names_from = County, values_from = n, values_fill = list(n = 0)) %>%
@@ -61,26 +78,27 @@ plateTypeModuleServer <- function(id, data) {
         mutate(Total = sum(Count), Percentage = Count / Total * 100) %>%
         ungroup()
       
-      # Create the plot
+      # Create the plot with renamed counties
       p <- ggplot(summarized_data, aes(x = County, y = Percentage, fill = Type,
-                                       text = paste(Type, ":Total Vehicle Type: ", round(Percentage, 2), "%"))) +
+                                       text = paste(Type, "Vehicle: ", round(Percentage, 2), "%"))) +
         geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
         scale_fill_brewer(palette = "Paired") +
-        labs(x = "Violation County", y = "Percentage", title = "Top Five Plate Types by Violation County") +
+        labs(x = "Violation County", y = "Percentage", title = "Comparison among Plate Types") +
         theme_minimal() +
         theme(axis.text.x = element_text(angle = 65, hjust = 1))
       
-      # Convert the ggplot object to a plotly object
+      # Convert the ggplot object to a plotly object with tooltips
       ggplotly(p, tooltip = c("text"))
     })
   })
 }
+
 ui <- fluidPage(
   plateTypeModuleUI("regState")
 )
+
 server <- function(input, output, session) {
   plateTypeModuleServer("regState")
 }
-
 
 shinyApp(ui = ui, server = server)
